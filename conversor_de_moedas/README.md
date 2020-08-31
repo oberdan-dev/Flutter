36e387d0028e26e3c10ec90ea1940e27515ced87
oberdan-dev <ocsantosdev@gmail.com>
2020-08-31 18:01:15 -0300

    APP #03 - Conversor de Moedas

---
+++ conversor_de_moedas/README.md
@@ -0,0 +1,10 @@
+# conversor_de_moedas
+
+App #3 - Conversor de Moedas
+
+Conceitos utilizados:
+
+- JSON;
+- API;
+- Future;
+- FutureBuilder
---
+++ conversor_de_moedas/android/app/src/main/kotlin/br/com/ocsantos/conversor_de_moedas/MainActivity.kt
@@ -0,0 +1,6 @@
+package br.com.ocsantos.conversor_de_moedas
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
