76f007b3053e7d686cad7ec9baf51a51f1133eb0
oberdan-dev <ocsantosdev@gmail.com>
2020-09-01 15:41:57 -0300

    APP #05 - Buscador de GIFs

---
+++ buscador_de_gifs/README.md
@@ -0,0 +1,16 @@
+# buscador_de_gifs
+
+App #5 - Buscador de GIFs
+
+Conceitos utilizados:
+
+- Navigator;
+- GridView;
+- Image Network;
+- GestureDetector;
+- ProgressIndicator;
+- Share;
+- FadeInImage;
+- Plugins
+
+
---
+++ buscador_de_gifs/lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:buscador_de_gifs/ui/gifPage.dart';
+import 'package:flutter/material.dart';
+
+import 'package:buscador_de_gifs/ui/homepage.dart';
+
+void main() {
+  runApp(MaterialApp(
+    home: HomePage(),
+    theme: ThemeData(
+        hintColor: Colors.white,
+        primaryColor: Colors.white,
+        inputDecorationTheme: InputDecorationTheme(
+          enabledBorder:
+              OutlineInputBorder(borderSide: BorderSide(color: Colors.white)),
+          focusedBorder:
+              OutlineInputBorder(borderSide: BorderSide(color: Colors.white)),
+          hintStyle: TextStyle(color: Colors.white),
+        )),
+  ));
+}
